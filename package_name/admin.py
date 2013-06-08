d9517c87946e34ffefeb00405b35847429eee4c4
Martin Brochhaus <mbrochh@gmail.com>
2013-06-08 20:47:18 +0800

    Added some sample admin / urls / views files

---
+++ package_name/admin.py
@@ -0,0 +1,11 @@
+"""Admin classes for the VAR_PACKAGE_NAME app."""
+# from django.contrib import admin
+
+# from . import models
+
+
+# class YourModelAdmin(admin.ModelAdmin):
+#    list_display = ['some', 'fields', ]
+#    search_fields = ['some', 'fieds', ]
+
+# admin.site.register(models.YourModel, YourModelAdmin)
---
+++ package_name/views.py
@@ -0,0 +1,8 @@
+"""Views for the VAR_PACKAGE_NAME app."""
+# from django.views.generic import TemplateView
+
+# from . import models
+
+
+# class YourView(TemplateView):
+#    template_name = 'VAR_PACKAGE_NAME/default.html'
