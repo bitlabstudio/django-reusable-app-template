7307d3a7c7015120ec5d5dd95ecbf2e219498e10
Martin Brochhaus <mbrochh@gmail.com>
2014-11-29 23:01:01 +0800

    Small syntax change in templatetags boilerplate file

--- template/package_name/templatetags/package_name_tags.py
+++ template/package_name/templatetags/package_name_tags.py
@@ -5,8 +5,8 @@ register = template.Library()
 
 #@register.filter
 #def lower(value):
-#    '''
+#    """
 #    Converts a string into all lowercase
-#    '''
+#
+#    """
 #    return value.lower()
-    
